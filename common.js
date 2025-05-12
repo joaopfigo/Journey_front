eb6a21b0ed4e722072b1e382a7592a02a0c06d0a
João Pedro Figueiredo <joaopedrofvalle@gmail.com>
2025-05-12 03:03:28 -0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,24 @@
+// common.js
+function getCurrentUser() {
+    return JSON.parse(sessionStorage.getItem("currentUser"));
+  }
+  
+  function getUserKey(baseKey) {
+    const user = getCurrentUser();
+    if (!user) return baseKey;
+    return baseKey + "_" + user.email;
+  }  
+  
+  // Por exemplo, ao trabalhar com colaboradores:
+  function loadColaboradores() {
+    const key = getUserKey("colaboradores");
+    const colaboradores = JSON.parse(localStorage.getItem(key) || "[]");
+    // ... atualize a interface com os colaboradores
+    return colaboradores;
+  }
+  
+  function saveColaboradores(colaboradores) {
+    const key = getUserKey("colaboradores");
+    localStorage.setItem(key, JSON.stringify(colaboradores));
+  }
+  
